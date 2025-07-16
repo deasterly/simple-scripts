f0b32b7a87bad898c6a389a0f49aa865459a9ba3
Dave Easterly <36435980+deasterly@users.noreply.github.com>
2025-07-16 10:32:57 -0600

    Improved commentary for clarity

--- script8.sh
+++ script8.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Loop through positional parameters
 
+# In addition to using $0 for the script itself and $1 or $2 and so on for arguments we have two more useful paramaters
+# We use $# to get the total number of arguments from the command line
+# We use $@ to list all the positional parameters' values in order, based on the command line arguments
+
 if [ $# -eq 0 ]; then
 	echo "$0 requires 1 or more arguments."
 	exit 1 # NO ARGS
@@ -9,9 +13,12 @@ fi
 if [ $# -eq 1 ]; then
 	echo "The only argument was $1"
 else
-	for V in $@; do
-		echo "This iteration the argument was $V"
-	done
+        for V in $@; do
+                echo "This iteration the argument sets the value of V to $V"
+        done
 fi
+
+echo 'Running `echo $@` returns the list used as values for $V in the `for` loop above.'
+echo $@
 
 exit 0
