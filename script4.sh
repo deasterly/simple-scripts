68ca668734b8cf6242f59b0b009fa0635b9d75b7
Dave Easterly <36435980+deasterly@users.noreply.github.com>
2025-07-16 09:58:43 -0600

    Improved commentary for clarity

--- script4.sh
+++ script4.sh
@@ -2,9 +2,13 @@
 
 # Use conditionals and tests
 
+# Open conditional with `if` and close the conditional with if backwards - `fi`
+# Tests are inside the square brackets - see `help test` to get started
 if [ $USER != "root" ] ; then 
 	echo "Hello, ${USER}."
 	exit 0
+# If the previous condition is TRUE the script ends without error
+# We use `else` as a "catchall" if previous conditions are all FALSE
 else
 	echo "Greetings, Administrator."
 	echo "This command isn't for you."
