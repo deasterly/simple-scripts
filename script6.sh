be3baf18eb68bcacd07c1ab182709175716fd404
Dave Easterly <36435980+deasterly@users.noreply.github.com>
2025-07-16 09:48:29 -0600

    Improved commentary for clarity

--- script6.sh
+++ script6.sh
@@ -7,19 +7,24 @@ echo -n "Enter the name of an animal: "
 read ANIMAL
 echo -n "The ${ANIMAL} has "
 
+# We can use `case` similar to if/then;elif/then; and else 
+# See `help case` for an explanation of the syntax
 case $ANIMAL in
 
 	horse | dog | cat)
 		echo -n "four";;
-
+		# If this condition is matched, we exit the `case/esac` and continue onwards
 	man | kangaroo)
 		echo -n "two";;
-
+		# This second condition is similar to an `elif` 
 	spider | tick)
 		echo -n "eight";;
-
+		# This is like another `elif`
 	*)
 		echo -n "an unknown number of";;
+  		# The *) in `case/esac` is a "catchall" condition similar to `else`
 esac
+# Close with case backwards to end the conditional
+
 echo " legs."
 
